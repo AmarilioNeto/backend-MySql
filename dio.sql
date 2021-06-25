49b68ec482a598512f15dfb11d65b76099fe649e
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2021-06-24 21:06:30 -0300

    Add files via upload

---
+++ dio.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pessoa (
+    id INT NOT NULL PRIMARY KIY AUTOINCREMENT,
+    nome VARCHAR NOT NULL,
+    nascimento DATE
+)
+INSERT INTO (nome, nascimento) VALUES ('Amarilio', '1988-07-03')
+INSERT INTO (nome, nascimento) VALUES ('Adila', '1988-12-09')
+INSERT INTO (nome, nascimento) VALUES ('liana', '1989-06-29')
