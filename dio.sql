728a824d48c1c19020b95c50a4704449b7aacc57
AmarilioNeto <80914219+AmarilioNeto@users.noreply.github.com>
2021-06-24 23:02:34 -0300

    Add files via upload

--- dio.sql
+++ dio.sql
@@ -3,6 +3,20 @@ CREATE TABLE pessoa (
     nome VARCHAR NOT NULL,
     nascimento DATE
 )
-INSERT INTO (nome, nascimento) VALUES ('Amarilio', '1988-07-03')
-INSERT INTO (nome, nascimento) VALUES ('Adila', '1988-12-09')
-INSERT INTO (nome, nascimento) VALUES ('liana', '1989-06-29')
+/*inserir dados*/
+INSERT INTO (nome, nascimento) VALUES ('Amarilio', '1988-07-03');
+INSERT INTO (nome, nascimento) VALUES ('Adila', '1988-12-09');
+INSERT INTO (nome, nascimento) VALUES ('liana', '1989-06-29');
+
+/* alterando dados */
+UPDATE pessoa SET nome= 'Amarilio Neto' WHERE id= 01
+
+/* deletando dados*/
+DELETE FROM pessoa WHERE id= 01
+
+/* orenando dados*/
+SELECT * FROM pessoa ORDER BY nome
+SELECT * FROM pessoa ORDER BY nome DESC
+
+/* agrupando dados*/
+SELECT COUNT(id), genero FROM pessoa GROUP BY genero
